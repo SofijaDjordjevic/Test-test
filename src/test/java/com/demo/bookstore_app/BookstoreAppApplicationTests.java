8a1016475af7a80f67b3d66131c0387a6f1439b2
SofijaDjordjevic <sofija.djordjevic98@gmail.com>
2025-11-22 16:38:04 +0100

    Updated files

--- src/test/java/com/demo/bookstore_app/BookstoreAppApplicationTests.java
+++ src/test/java/com/demo/bookstore_app/BookstoreAppApplicationTests.java
@@ -3,8 +3,11 @@ package com.demo.bookstore_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class BookstoreAppApplicationTests {
 
 	@Test
--- src/test/java/com/demo/bookstore_app/repo/BookRepoTest.java
+++ src/test/java/com/demo/bookstore_app/repo/BookRepoTest.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
+@ActiveProfiles("test")
+
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookRepoTest
 {
